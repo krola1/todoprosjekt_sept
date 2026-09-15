917995deb6cd7f336f4a8b002e756a61d907f7c1
krola1 <kwl082024@gmail.com>
2026-09-15 09:57:41 +0200

    bang

---
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -0,0 +1,11 @@
+import styles from "./TodoFilters.module.css";
+
+function TodoFilters() {
+  return (
+    <div className={styles.wrapper}>
+      <h2>TodoFilters</h2>
+    </div>
+  );
+}
+
+export default TodoFilters;
